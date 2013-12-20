215ee8a799367d8ca10a5b53d40e0388315710af
Erick <erick.castillo@cafestudioweb.com>
2013-12-20 12:49:17 -0600

    NOT WORKING: XML READER

--- BigSum/src/TestXMLFileToDOM.java
+++ BigSum/src/TestXMLFileToDOM.java
@@ -1,4 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 
 public class TestXMLFileToDOM {
 
+	public static final String TEST_FILE_PATH = "../test_file.xml";
+	
+	XMLFileToDOM xmlParser;
+	
+	public TestXMLFileToDOM(){
+		xmlParser = new XMLFileToDOM(TEST_FILE_PATH);
+	}
+	
+	@Test
+	public void testGetTestNode(){
+		String expected = "test";
+		String actual = xmlParser.getNodesByTag("test").item(0).getNodeName();
+		assertEquals("NODE_ERROR: Not getting the expected node",actual,expected);
+	}
+	
+	@Test
+	public void testGetNotExistingNode(){
+		int expected = 0;
+		int actual = xmlParser.getNodesByTag("not_existing").getLength();
+		assertEquals("NOT_EMPTY_NODE: Not getting the expected node",actual,expected);
+	}
+	
+	@Ignore
+	@Test
+	public void testGetNotExistingNodeChilds(){
+		/*NodeList expected = null;
+		//NodeList actual = xmlParser.getChildNodes("not_existing");
+		assertEquals("NOT_EMPTY_NODE_CHILD_ERROR: Not getting the expected node",
+				actual,expected);*/
+	}
+	
+	@Test
+	public void testGetTestChildNodes(){
+		Node childs = xmlParser.getChildNodes("test");
+		int expected = 2;
+		System.out.println(childs.getNodeValue());
+		int actual = 1;//childs.getLength();
+		assertEquals("NOT_CORRECT_CHILDS_ERROR: Not getting the expected childs",expected,actual);
+		/*String child1_expected_value = "1";
+		String child2_expected_value = "2";
+		String child1_actual_value = childs.item(0).getNodeValue();
+		String child2_actual_value = childs.item(1).getNodeValue();
+		assertEquals("NODE_ERROR: Not getting the expected node",child1_actual_value,child1_expected_value);
+		assertEquals("NODE_ERROR: Not getting the expected node",child2_actual_value,child2_expected_value);*/
+	}
+	
+	
 }
--- BigSum/src/XMLFileToDOM.java
+++ BigSum/src/XMLFileToDOM.java
@@ -1,4 +1,43 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 
 public class XMLFileToDOM {
+
+	Document document;
+	
+	public XMLFileToDOM(String file) {
+		try{
+			File xmlFile = new File(file);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			document = dBuilder.parse(xmlFile);
+			//document.getDocumentElement().normalize();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	public NodeList getNodesByTag(String tag) {
+		return document.getLastChild().getElementById(tag). .getElementsByTagName(tag);
+	}
+
+	public Node getChildNodes(String tag) {
+		Node parent = getNodesByTag(tag).item(0);
+		if(parent != null){
+			return parent.getChildNodes().item(0);
+		}
+		else{
+			return null;
+		}
+	}
+
 
 }
