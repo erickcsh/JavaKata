88e01ef0fb74e61b8f214cb2a3a76714e245bbac
Erick <erick.castillo@cafestudioweb.com>
2013-12-20 12:47:18 -0600

    Test for Big Sum Refactored

--- BigSum/src/TestBigSumCalculator.java
+++ BigSum/src/TestBigSumCalculator.java
@@ -12,12 +12,12 @@ public class TestBigSumCalculator {
 	
 	BigSumCalculator bsCalculator;
 	public TestBigSumCalculator(){
-		ArrayList list = createBigIntArray();
+		ArrayList<BigInteger> list = createBigIntArray();
 		this.bsCalculator = new BigSumCalculator(list);
 	}
 	
 	private ArrayList<BigInteger> createBigIntArray(){
-		ArrayList list = new ArrayList<BigInteger>();
+		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
 		for(int i = 0; i < 10; i++){
 			list.add(new BigInteger("111222333444555666").add(BigInteger.ONE));
 		}
@@ -28,20 +28,20 @@ public class TestBigSumCalculator {
 	public void testSumArrayList(){
 		BigInteger actual = this.bsCalculator.sumAll();
 		BigInteger expected = new BigInteger("1112223334445556670");
-		assertEquals("SUMERROR: Not the expected sum",actual,expected);
+		assertEquals("SUM_ERROR: Not the expected sum",actual,expected);
 	}
 	
 	@Test
 	public void testGetFirst0SumNumbers(){
 		String expected = new BigInteger("1112223334445556670").toString().substring(0, 0);
 		String actual = this.bsCalculator.getFirstNumbersOfSum(0);
-		assertEquals("SUBSUMERROR: Not the expected sum digits",actual,expected);
+		assertEquals("SUB_SUM_ERROR: Not the expected sum digits",actual,expected);
 	}
 	
 	@Test
 	public void testGetFirst10SumNumbers(){
 		String expected = new BigInteger("1112223334445556670").toString().substring(0, 10);
 		String actual = this.bsCalculator.getFirstNumbersOfSum(10);
-		assertEquals("SUBSUMERROR: Not the expected sum digits",actual,expected);
+		assertEquals("SUB_SUM_ERROR: Not the expected sum digits",actual,expected);
 	}
 }
